0d8037d5192d9adcfb760eb3c860929abb5a4fa6
tapiwa <tapiwambizvo@gmail.com>
2024-09-23 10:34:48 +0200

    pyhton version 3.12

--- lambda-api-gateway/main.tf
+++ lambda-api-gateway/main.tf
@@ -61,7 +61,7 @@ resource "aws_lambda_function" "hello_world" {
   s3_bucket = aws_s3_bucket.lambda_bucket.id
   s3_key    = aws_s3_object.lambda_hello_world.key
 
-  runtime = "python3.6"
+  runtime = "python3.12"
   handler = "api.handler"
 
   source_code_hash = data.archive_file.lambda_hello_world.output_base64sha256
