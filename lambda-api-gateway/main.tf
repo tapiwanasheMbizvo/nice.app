026b1fcebe4d4a5ea992e139fe950640c476e0c3
tapiwa <tapiwambizvo@gmail.com>
2024-09-23 10:31:10 +0200

    create lambda function

--- lambda-api-gateway/main.tf
+++ lambda-api-gateway/main.tf
@@ -51,3 +51,48 @@ resource "aws_s3_object" "lambda_hello_world" {
 
   etag = filemd5(data.archive_file.lambda_hello_world.output_path)
 }
+
+
+# lambda function def
+
+resource "aws_lambda_function" "hello_world" {
+  function_name = "helloapipy"
+
+  s3_bucket = aws_s3_bucket.lambda_bucket.id
+  s3_key    = aws_s3_object.lambda_hello_world.key
+
+  runtime = "python3.6"
+  handler = "api.handler"
+
+  source_code_hash = data.archive_file.lambda_hello_world.output_base64sha256
+
+  role = aws_iam_role.lambda_exec.arn
+}
+
+resource "aws_cloudwatch_log_group" "hello_world" {
+  name = "/aws/lambda/${aws_lambda_function.hello_world.function_name}"
+
+  retention_in_days = 30
+}
+
+resource "aws_iam_role" "lambda_exec" {
+  name = "serverless_lambda"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Sid    = ""
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
