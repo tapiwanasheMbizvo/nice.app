a2f8f8512dbf93874099468c5ee86761babb518d
tapiwa <tapiwambizvo@gmail.com>
2024-09-23 12:41:47 +0200

    added GET message to py

--- lambda-api-gateway/api/api.py
+++ lambda-api-gateway/api/api.py
@@ -4,21 +4,24 @@ import random
 def lambda_handler(event, context):
     txn_id = "none"
     message = "failed"
+    routeKey = event.get('routeKey').split()[0]
     
-    try:
-        body = event['body']
-        body_dict = json.loads(body)
-        acc_number = body_dict.get('ACC_NUMBER')
-        
-        if acc_number:
-            txn_id = random.getrandbits(128).to_bytes(16, 'little').hex()
-
-            # do some db stuff , and return the message 
-            message = "Transaction processed successfully!!!"
-    except (KeyError, json.JSONDecodeError):
-        txn_id = "ERROR"
-        message = "Invalid input or JSON format."
-
+    if routeKey =='POST':
+        try:
+            body = event['body']
+            body_dict = json.loads(body)
+            acc_number = body_dict.get('ACC_NUMBER')
+            
+            if acc_number:
+                txn_id = random.getrandbits(128).to_bytes(16, 'little').hex()
+    
+                # do some db stuff , and return the message 
+                message = "Transaction processed successfully!!!"
+        except (KeyError, json.JSONDecodeError):
+            txn_id = "ERROR"
+            message = "Invalid input or JSON format."
+    elif routeKey == 'GET':
+        message = "Get Method not defined"
     return {
         'statusCode': 200,
         'body': json.dumps({
