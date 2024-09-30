cc57ebf3ce4f0341e54be490e106ff96044b920f
tapiwanasheMbizvo <tapiwambizvo@gmail.com>
2024-09-30 07:48:36 +0200

    added how are you

--- src/main/java/com/tapiwa/nice/app/controllers/GreetingController.java
+++ src/main/java/com/tapiwa/nice/app/controllers/GreetingController.java
@@ -17,7 +17,7 @@ public class GreetingController {
     @GetMapping
     public ResponseEntity<Greeting> getGreeting(){
 
-        var gretting = new Greeting("Hello There Buddy!!", LocalDateTime.now());
+        var gretting = new Greeting("Hello There Buddy, How are you ", LocalDateTime.now());
 
         return  ResponseEntity.ok(gretting);
     }
